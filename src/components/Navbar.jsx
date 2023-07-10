6ee91582bec195512e729a74269ca0e9e8e6e299
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-07-10 12:02:47 +0530

    Finalize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {FaBars, FaTimes} from "react-icons/fa"
+import { Link } from 'react-scroll'
 
 
 
@@ -10,23 +11,23 @@ const Navbar = () => {
     const links = [
         {
             id: 1,
-            link: "Home"
+            link: "home"
         },
         {
             id: 2,
-            link: "About"
+            link: "about"
         },
         {
             id: 3,
-            link: "Portfolio"
+            link: "portfolio"
         },
         {
             id: 4,
-            link: "Experience"
+            link: "experience"
         },
         {
             id: 5,
-            link: "Contact"
+            link: "contact"
         },
     ]
 
@@ -37,7 +38,7 @@ const Navbar = () => {
             <h1 className='text-5xl font-signature ml-2'>Abhinav</h1>
         </div>
         <ul className='hidden md:flex'>
-            {links.map(({id,link})=><li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>{link}</li>)}
+            {links.map(({id,link})=><li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'><Link to={link} smooth duration={500}>{link}</Link></li>)}
         </ul>
 
         <div onClick={()=> setNav((prevstate)=>!prevstate)} className='md:hidden cursor-pointer pr-4 z-10 text-gray-500'>
@@ -45,7 +46,7 @@ const Navbar = () => {
         </div>
 
         {nav && <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-            {links.map(({id,link})=><li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>{link}</li>)} 
+            {links.map(({id,link})=><li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={()=> setNav(!nav)} to={link} smooth duration={500}>{link}</Link></li>)} 
         </ul>}
         
     </div>
