65a59e32052746ed893ed3d84aff92039f208eb1
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-07-10 16:32:46 +0530

    Fix for smaller height devices

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,16 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div
-      name="home"
-      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
-    >
-      <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
-        <div className="flex flex-col justify-center h-full">
+    <div name="home" className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+        <div className="flex flex-col justify-center h-full mt-20 md:mt-0">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Frontend Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            I have 8 months of experience building and designing software.
+            Currently, I love to work on web applications using technologies like
+            React, Tailwind, Next JS, and MaterialUI.
           </p>
           <div>
             <Link
